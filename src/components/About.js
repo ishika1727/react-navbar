ac6fe83d15053ffea67ba3c3358fb896177c5763
anukrati1507 <saxena.anukrati1507@gmail.com>
2020-07-04 16:47:35 +0530

    basic links throug navbar

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Rainbow from '../hoc/rainbow'
+const About = () => {
+    return (
+        <div className="container">
+        <h4 className="center">About</h4>
+        <p>lorem ipsum</p>
+        </div>
+    )
+}
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { checkPropTypes } from 'prop-types';
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // },2000);
+    return (
+        <div className="container">
+        <h4 className="center">Contact</h4>
+        <p>lorem import ipsum</p>
+        </div>
+    )
+}
+export default Contact
